ae345f817c4c607281ca2dd278c94c45c588aad4
Lahari-nagaraj <158993362+Lahari-nagaraj@users.noreply.github.com>
2025-02-03 23:18:34 +0530

    added feedback section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,29 +1,94 @@
-import { Tilt } from "react-tilt"
-import { motion } from "framer-motion"
-import { styles } from "../styles"
-import { github } from "../assets"
-import { SectionWrapper } from "../hoc"
-import { projects } from "../constants"
-import { fadeIn, textVariant} from '../utils/motion';
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { p } from "framer-motion/client";
+
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[350px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              className="black-gradient w-8 h-8 rounded-full flex justify-center items-center cursor-pointer"
+              onClick={() => window.open(source_code_link, "_blank")}
+            >
+              <img
+                src={github}
+                alt="github"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h3 className="text-white text-[24px] font-bold">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+          
+        </div>
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color} `} >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
 
 const Works = () => {
   return (
     <>
-    <motion.div variants={textVariant()}>
-            <p className={styles.sectionSubText}>My work!</p>
-            <h2 className={styles.sectionHeadText}>Projects</h2>
-          </motion.div>
-    
-    <div className="w-full flex">
-      <motion.p variants={fadeIn("", "", 0.1,1)}
-      className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Work</p>
+        <h2 className={styles.sectionHeadText}>Projects</h2>
+      </motion.div>
 
-      </motion.p>
+      {/* Description Section */}
+      <div className="w-full flex flex-col items-center">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-4 text-secondary text-[20px] w-full max-w-5xl text-center px-10 leading-[32px]"
+        >
+          Following projects showcase my skill set. I am an aspiring software
+          developer focusing on web development and exploring the field of AI.
+        </motion.p>
 
-    </div>
-    
+        {/* Project Cards */}
+        <div className="mt-20 flex flex-wrap gap-10 justify-center w-full">
+          {projects.map((project, index) => (
+            <ProjectCard key={`project-${index}`} index={index} {...project} />
+          ))}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default SectionWrapper(Works, "");
